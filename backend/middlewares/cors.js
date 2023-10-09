84a5de7c087b7a62f9c462031cccdb66bf615cb3
SuperAlex2013 <122688147+SuperAlex2013@users.noreply.github.com>
2023-10-10 01:55:50 +0300

    some fixes

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,5 +1,5 @@
 const { NODE_ENV, ALLOWED_CORS_PRODUCTION } = process.env;
-const ALLOWED_CORS = NODE_ENV === 'production' ? ALLOWED_CORS_PRODUCTION.split(', ') : ['https://localhost:3000', 'http://localhost:3000', 'localhost:3000'];
+const ALLOWED_CORS = NODE_ENV === 'production' ? ALLOWED_CORS_PRODUCTION.split(', ') : ['https://ulra.nomoredomainsrocks.ru/', 'http://ulra.nomoredomainsrocks.ru/', '127.0.0.1:3000'];
 
 const CORS_OPTIONS = {
   credentials: true,
